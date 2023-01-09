b2b7c317a8a96637cf633a4d391deb0e14cfda37
pankajbaba33 <py796880@gmail.com>
2023-01-09 13:32:14 +0530

    ass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter,  Route, Routes } from "react-router-dom"
+import Header from './Header';
+import Navbar from './Navbar';
+import Photo from './componenet/Photo';
+import Candle from './componenet/Candle';
+import Check from './componenet/Check';
+import Reveal from './componenet/Reveal';
+import Soap from './componenet/Soap';
+import Special from './componenet/Special';
+import Tart from './componenet/Tart';
+import Rep from './componenet/Rep';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<BrowserRouter>
+     <Header/>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Photo/>}/>
+      <Route path='candle' element={<Candle/>}/>
+      <Route path='check' element={<Check/>}/>
+      <Route path='rep' element={<Rep/>}/>
+      <Route path='reveal' element={<Reveal/>}/>
+      <Route path='soap' element={<Soap/>}/>
+      <Route path='special' element={<Special/>}/>
+      <Route path='Tart' element={<Tart/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
